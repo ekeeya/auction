7629814874c41c49e3dae4e7dc614dc27aa1c16c
rodrickcalvin <rodrickwamala@gmail.com>
2023-03-05 18:12:15 +0300

    Sets up skycatle UI project

---
+++ skycastle_UI/skycastle/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        SKYCASTLE AUCTION
+      </header>
+    </div>
+  );
+}
+
+export default App;
